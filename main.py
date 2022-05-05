c1bbfe293c3efd5d3f05fcdf2d409b7d15d6a6a5
Mike <ms53150@gmail.com>
2022-05-04 20:55:08 -0400

    Add try-except for create/open .json file

--- main.py
+++ main.py
@@ -43,16 +43,21 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please fill all fields.")
     else:
-        with open("data.json", "r") as data_file:
-            # Read old data
-            data = json.load(data_file)
-            # Update old data with new data
+        try:
+            with open("data.json", "r") as data_file:
+                # Read old data
+                data = json.load(data_file)
+                # Update old data with new data
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             data.update(new_data)
 
-        with open("data.json", "w") as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_input.delete(0, "end")
             password_input.delete(0, "end")
 
